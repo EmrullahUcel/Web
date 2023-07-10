e1d1ecd1756325fad7c42ef94a9eacfa3695d743
EmrullahUcel <emrullahucel@gmail.com>
2023-07-10 10:54:44 +0300

    dark light mode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,72 @@
-import Header from "./components/Header"
-import MainSection from "./components/MainSection"
-import { Route, Routes } from 'react-router-dom'
-import About from "./components/pages/About"
-import Contact from "./components/pages/Contact"
-import BackPage from './components/BackPage'
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import BackPage from "./components/BackPage";
+import { useReducer } from "react";
+import { FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 
+const initialState = {
+  bgColor: "white",
+  color: "black",
+};
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "dark":
+      return { bgColor: "rgb(62,55,55)", color: "white" };
+    case "light":
+      return { bgColor: "white", color: "black" };
+  }
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const handleDark = (e) => {
+    const buton = e.target.checked;
+    if (buton === true) {
+      dispatch({ type: "dark" });
+      e.style = "none"
+    }else 
+    dispatch({ type: "light" });
+  };
+  const handleLight = () => {
+    
+  };
   return (
-
-    <div className='pc:w-full pc:h-full pc:flex pc:flex-col pc:items-center pc:justify-center
-    phone:flex justify-center phone:items-center
-    '>
-      <BackPage>
+    <div
+      style={{
+        transition:"all 2s",
+        backgroundColor: state.bgColor,
+        color: state.color,
+      }}
+      className="pc:w-full pc:h-full pc:flex pc:flex-col pc:items-center pc:justify-center pc:text-base
+    phone:flex phone:flex-col justify-center phone:items-center phone:text-xs
+    "
+    >
+      <div className="flex mt-5 w-1">
+        <label  htmlFor="dark">
+          <input
+            id="dark"
+            type="checkbox"
+            className="hidden"
+            onChange={handleDark}
+          />
+          <FaMoon className="moon absolute" />
+          <FaSun className="sun absolute" />
+        </label>
+        <label htmlFor="light">
+          <input
+            id="light"
+            type="checkbox"
+            className="hidden"
+            onChange={handleLight}
+          />
+        </label>
+      </div>
+      <BackPage state={state}>
         <Header />
         <Routes>
           <Route path="/" element={<MainSection />} />
@@ -21,8 +75,7 @@ const App = () => {
         </Routes>
       </BackPage>
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,33 @@
-import { Link, NavLink } from 'react-router-dom'
-
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        
-        <nav className='flex justify-between mt-5'>
-            <div className='flex'>
-               
-            
-            </div>
-            <div className='pc:flex pc:flex-col pc:mr-5 pc:m-[-20px]
-            phone:flex phone:m-[-20px] '>
-                <Link className='hover:scale-125 ease-in duration-200 phone:mx-1' exact to="/">Home</Link>
-                <Link className='hover:scale-125 ease-in duration-200 phone:mx-1'  to="/About">About Me</Link>
-                <Link className='hover:scale-125 ease-in duration-200 phone:mx-1'  to="Contact">Contact</Link>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className=" mt-5 pc:justify-end pc:flex">
+      <div
+        className="pc:flex pc:flex-col pc:mr-5 pc:m-[-20px]
+            phone:flex phone:m-[-20px] text- justify-center items-center"
+      >
+        <Link
+          className="hover:scale-125 ease-in duration-200 phone:mx-1"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="hover:scale-125 ease-in duration-200 phone:mx-1"
+          to="/About"
+        >
+          About Me
+        </Link>
+        <Link
+          className="hover:scale-125 ease-in duration-200 phone:mx-1"
+          to="Contact"
+        >
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
